b35e22f50adf25e99f9d4d10361a8df54ba95d62
Gaurab <gaurabkus781@gmail.com>
2025-02-09 21:56:29 +0530

    Create ArrayFunction and add display method to display the array

---
+++ ArrayFunctions.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class ArrayFunctions{
+     // Display an ArrayList of integers
+    void display(ArrayList<Integer> array) {
+        System.out.println("ArrayList: " + array);
+    }
+}
